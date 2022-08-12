fbaada743316a8edc5b477a5862b9d401a0e28b8
FathaGhaniAlRauf <ghanirauf312@gmail.com>
2022-08-12 20:07:46 +0700

    menambah batch file

---
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/cari-java.bat
@@ -0,0 +1,7 @@
+@echo OFF
+
+cd C:\Users\ghani\Desktop\Coding-PGT\kemampuan-dasar\kemampuan-dasar-1\kasus
+
+DIR *.java
+echo Ada file Java pada direktori C:\Users\ghani\Desktop\Coding-PGT\kemampuan-dasar\kemampuan-dasar-1\kasus
+PAUSE
---
+++ kemampuan-dasar/kemampuan-dasar-1/kasus/file-ren.bat
@@ -0,0 +1,3 @@
+@echo OFF
+
+set /p input=" file tes.java ditemukan, Ingin mengganti nama (y/n) ? "
